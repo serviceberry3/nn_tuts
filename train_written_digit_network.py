33139f042ffa4eda426ce247c5e6ee926afea733
Noah Weiner <noah.weiner@yale.edu>
2020-11-11 12:50:18 -0500

    vectorisation of ground truth digits

--- train_written_digit_network.py
+++ train_written_digit_network.py
@@ -28,7 +28,7 @@ from sklearn.preprocessing import StandardScaler
 X_scale = StandardScaler()
 
 #scale the data to fit between -2 and 2 (subtract mean and div by stdev)
-X=X_scale.fit_transform(digits.data)
+X = X_scale.fit_transform(digits.data)
 
 #make sure data was scaled
 print(X[0,:])
@@ -45,6 +45,38 @@ y = digits.target
 
 #randomly separate training and testing sets: make training 60% of data, testing 40%
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+
+'''Need output layer to predict whether digit represented by input pixels is between 0 and 9. Sensible nn architecture for this
+would be to have output layer of 10 nodes, each representing a digit from 0-9. Want to train network so that when image of the
+digit “5” is presented, node in output layer representing 5 has highest value. Ideally, output like this:
+[0, 0, 0, 0, 0, 1, 0, 0, 0, 0]. But can settle for something like:
+[0.01, 0.1, 0.2, 0.05, 0.3, 0.8, 0.4, 0.03, 0.25, 0.02] and take max index of output array and call that our predicted digit.
+For MNIST data supplied in scikit learn dataset, the “targets” or classification of the handwritten digits is in form of
+single number. Need to convert that num into vector so it lines up with our 10 node output layer. In other words, if
+target val in dataset is “1”, want to convert it into: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0].'''
+import numpy as np
+
+#converts 1D array of correct digits to a 2D array with digits replaced as 1D arrays as shown in above blurb
+def convert_y_to_vect(y):
+    #intialize output vector to 2D array of zeroes
+    y_vect = np.zeros((len(y), 10))
+
+    #iterate over all digits
+    for i in range(len(y)):
+        #set the appropriate slot in this digit's vector to 1
+        y_vect[i, y[i]] = 1
+
+    return y_vect
+
+#convert training and test y datasets to vector representations of the digits
+y_v_train = convert_y_to_vect(y_train)
+y_v_test = convert_y_to_vect(y_test)
+
+
+#verify that the conversion worked
+print(y_train[0], y_v_train[0])
+
+
 
 
 
